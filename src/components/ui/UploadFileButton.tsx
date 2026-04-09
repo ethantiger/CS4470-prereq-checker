c12a29849ea65a387520b445aab9ac8105631562
Ethan Tiger Wakefield <ethanwakefield007@gmail.com>
2026-04-09 13:41:46 -0400

    add upload file button to student table

---
+++ src/components/ui/UploadFileButton.tsx
@@ -0,0 +1,52 @@
+import { useRef } from "react";
+import { useStudentActions } from "@/stores/useStudent";
+import { extractInfo } from '@/helpers/parseFile';
+import { IconFileUpload } from '@tabler/icons-react';
+import './UploadFileButton.css';
+
+export default function UploadFileButton() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { addStudent, clearStudents } = useStudentActions();
+
+  const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      alert('No file selected. Please choose a PDF file to upload.');
+      return;
+    }
+
+    clearStudents();
+
+    try {
+      const studentInfo = await extractInfo(file);
+      
+      for (const student of studentInfo) {
+        addStudent(student);
+      }
+      console.log(`Added ${studentInfo.length} students from PDF pages`);
+
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+    <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileUpload}
+        style={{ display: 'none' }}
+        multiple
+      />
+      <button
+        onClick={() => fileInputRef.current?.click()}
+        className="upload-btn"
+      >
+        <span><IconFileUpload size={24}/></span>Upload File
+      </button>
+    </>
+  )
+}
